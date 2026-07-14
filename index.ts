7332ccac66c808a46fda51eeeba910f9c632b83b
Abgal <hmdabgal@gmail.com>
2026-07-14 21:37:03 +0600

    cors problem

--- index.ts
+++ index.ts
@@ -12,7 +12,25 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 
-app.use(cors());
+// app.use(cors());
+const allowedOrigins = ['https://nexthire-client.vercel.app', 'http://localhost:3000'];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    // Allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+}));
+
+
+
+
 
 app.use(express.json());
 
@@ -62,9 +80,9 @@ const contactCollection = db.collection("contacts");
 
 
 
-    await client.db("admin").command({
-      ping:1
-    });
+    // await client.db("admin").command({
+    //   ping:1
+    // });
 
 
     console.log(
