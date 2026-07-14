c63713e68c9658428e65469b673ca13ad443155d
Abgal <hmdabgal@gmail.com>
2026-07-14 21:46:31 +0600

    cors problem

--- index.ts
+++ index.ts
@@ -12,21 +12,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 
-// app.use(cors());
-const allowedOrigins = ['https://nexthire-client.vercel.app', 'http://localhost:3000'];
-
-app.use(cors({
-  origin: function (origin, callback) {
-    // Allow requests with no origin (like mobile apps or curl requests)
-    if (!origin) return callback(null, true);
-    
-    if (allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  }
-}));
+app.use(cors());
 
 
 
